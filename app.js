5d1087ccaf5f4fef015720f79f3d37e66dab19da
jakhongiralijonov <jakhongirweb@gmail.com>
2022-02-24 18:56:27 +0500

    init changes

---
+++ app.js
@@ -0,0 +1,54 @@
+const countryName = document.getElementById('countName')
+const main = document.querySelector('.main-cont')
+const loader = document.querySelector('#loader')
+if (navigator.onLine) {
+    console.log('You are online use our web site !');
+} else {
+    alert('You are ofline');
+}
+
+
+const api = 'https://covid19.mathdro.id/api/countries/'
+
+$("#form").submit(function(event) {
+    event.preventDefault();
+    const value = $("#input").val()
+    countryName.innerHTML = value
+    requestApi(`${api}${value}`)
+})
+
+async function requestApi(url) {
+    loader.classList.remove('hidden')
+    try {
+        const req = await fetch(url)
+
+        if (!req.ok) {
+
+            if (!(req.status == 200)) {
+                alert('Davlat nomini tekshiring')
+
+            }
+            throw new Error('Xatolik mavjud')
+
+        }
+        const data = await req.json()
+        console.log(data);
+        getDatas(data)
+    } catch (err) {
+        console.log(err.message);
+
+    }
+}
+
+requestApi(api)
+const death = document.getElementById('death')
+const heal = document.getElementById('heal')
+const confirmed = document.getElementById('confirmed')
+
+function getDatas(data) {
+    loader.classList.add('hidden')
+    main.classList.remove('hidden')
+    confirmed.innerHTML = data.confirmed.value
+    death.innerHTML = data.deaths.value
+    heal.innerHTML = data.recovered.value
+}
